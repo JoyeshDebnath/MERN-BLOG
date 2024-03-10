b7e1e651ec3d76923615f9736efb0858b369426b
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2024-03-10 20:29:13 +0530

    pages and Routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
 const App = () => {
 	return (
-		<div>
-			<h1 className="text-3xl text-red-500">App </h1>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/projects" element={<Projects />} />
+			</Routes>
+		</BrowserRouter>
 	);
 };
 
