204059cc5aadde796008feed1be2703c666c1043
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2024-03-12 14:50:27 +0530

    Footer Component Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 const App = () => {
 	return (
 		<BrowserRouter>
@@ -20,6 +21,7 @@ const App = () => {
 				<Route path="/dashboard" element={<Dashboard />} />
 				<Route path="/projects" element={<Projects />} />
 			</Routes>
+			<Footer />
 		</BrowserRouter>
 	);
 };
